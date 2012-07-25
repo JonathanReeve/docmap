cb7ad6a2695ac2d1078db0c5d43081fa8f45122a
Jonathan Reeve <jon.reeve@gmail.com>
2012-07-25 16:23:41 -0400

    categorized tags in item view, kind of fixes #67

--- items/tags.php
+++ items/tags.php
@@ -11,7 +11,20 @@ head(array('title'=>$pageTitle,'bodyid'=>'items','bodyclass'=>'tags'));
         <?php echo custom_nav_items(); ?>
     </ul>
 
-    <?php echo tag_cloud($tags,uri('items/browse')); ?>
+<div id="tags_alpha" class="bigTagCloud">
+    <h2>All Tags Sorted Alphabetically</h2>
+    <?php 
+	$tags = get_tags(array('sort' => 'alpha'), null);
+	echo tag_cloud($tags,uri('items/browse')); 
+    ?>
+</div>
+<div id="tags_most" class="bigTagCloud">
+    <h2>All Tags Sorted by Frequency of Occurrence</h2>
+    <?php 
+	$tags = get_tags(array('sort' => 'most'), null);
+	echo tag_cloud($tags,uri('items/browse')); 
+    ?>
+</div>
 
 </div><!-- end primary -->
 
