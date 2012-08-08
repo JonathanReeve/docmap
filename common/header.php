6f4ab85002d8f19e07a33c8571b0a47b09dcfeca
Jonathan Reeve <jon.reeve@gmail.com>
2012-08-08 18:36:17 -0400

    added ability to customize header background image, fixes #73

--- common/header.php
+++ common/header.php
@@ -5,26 +5,21 @@
     <?php if ( $description = settings('description')): ?>
     <meta name="description" content="<?php echo $description; ?>" />
     <?php endif; ?>
-
     <title><?php echo settings('site_title'); echo isset($title) ? ' | ' . strip_formatting($title) : ''; ?></title>
-
     <?php echo auto_discovery_link_tags(); ?>
-
     <!-- Plugin Stuff -->
     <?php plugin_header(); ?>
-
     <!-- Stylesheets -->
     <?php
     queue_css('style');
     display_css();
     ?>
-
     <!-- JavaScripts -->
     <?php 
     queue_js('mosaic.1.0.1'); 
     display_js(); 
     ?>
-		<!-- This javascript snippet controls the mosaic overlay animation -->
+		<!-- This snippet controls the mosaic overlay animation -->
 		<script type="text/javascript">  
 			jQuery(function($){
 				$('.bar').mosaic({
@@ -32,30 +27,24 @@
 				});
 		    });
 		</script> 
-
+    <?php custom_header_background(); ?>
 </head>
-
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <?php plugin_body(); ?>
     <div id="wrap">
-
         <div id="header">
             <?php plugin_page_header(); ?>
             <div id="search-container">
                 <?php echo simple_search(); ?>
                 <?php echo link_to_advanced_search(); ?>
             </div><!-- end search -->
-
             <div id="site-title"><?php echo link_to_home_page(custom_display_logo()); ?></div>
-
         </div><!-- end header -->
-
         <div id="primary-nav">
             <ul class="navigation">
             <?php echo custom_public_nav_header(); ?>
             </ul>
         </div><!-- end primary-nav -->
         <?php echo custom_header_image(); ?>
-
         <div id="content">
             <?php plugin_page_content(); ?>
