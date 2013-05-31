b3000817b3250a52bd0c32dc05fb1cb98ebcac99
Jonathan Reeve <jon.reeve@gmail.com>
2013-05-31 19:02:30 -0400

    removed footer text, fixes #133
    it was apparently coming from certain exhibits, not Pinstripe

--- common/footer.php
+++ common/footer.php
@@ -7,7 +7,6 @@
             </ul>
 
             <div id="footer-text">
-                <?php echo get_theme_option('Footer Text'); ?>
                 <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = settings('copyright')): ?>
                     <p><?php echo $copyright; ?></p>
                 <?php endif; ?>
